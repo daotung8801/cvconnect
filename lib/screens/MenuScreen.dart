f30d5cc1c3d2771d5702671563bbbe7e86b1ea42
kieuvantuyen01 <tuyenkieuvan@gmail.com>
2022-04-21 04:45:55 +0700

    add bottom navigation bar

---
+++ lib/screens/MenuScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Menu Screen'),
+        ),
+      );
+}
---
+++ lib/screens/ReportScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ReportScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Report Screen'),
+        ),
+      );
+}
