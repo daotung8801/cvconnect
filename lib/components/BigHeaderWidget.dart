487fc795f0a2f71bdf8db89c867889bec4719095
kieuvantuyen01 <tuyenkieuvan@gmail.com>
2022-04-21 13:16:11 +0700

    change return type of BigHeaderWidget

--- lib/components/BigHeaderWidget.dart
+++ lib/components/BigHeaderWidget.dart
@@ -1,31 +1,33 @@
 import 'package:cvconnect/components/TitleText1.dart';
 import 'package:flutter/material.dart';
 
-class BigHeaderWidget extends StatelessWidget {
+class BigHeaderWidget extends StatelessWidget with PreferredSizeWidget {
   final String text;
 
   const BigHeaderWidget({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 0, top: 25),
-          child: TitleText1(
-            text: text,
-            fontFamily: 'Nunito Sans',
-            fontSize: 27,
-            fontWeight: FontWeight.bold,
-            r: 0,
-            g: 0,
-            b: 0,
-          ),
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Padding(
+        padding: const EdgeInsets.only(left: 0, top: 25),
+        child: TitleText1(
+          text: text,
+          fontFamily: 'Nunito Sans',
+          fontSize: 27,
+          fontWeight: FontWeight.bold,
+          r: 0,
+          g: 0,
+          b: 0,
         ),
       ),
-      //debugShowCheckedModeBanner: false,
     );
+    //debugShowCheckedModeBanner: false;
   }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(72);
 }
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -2,8 +2,12 @@ import 'package:cvconnect/components/BigHeaderWidget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => const BigHeaderWidget(
-        text: 'Trang chủ',
+  Widget build(BuildContext context) => const Scaffold(
+        appBar: BigHeaderWidget(
+          text: 'Trang chủ',
+        ),
       );
 }
