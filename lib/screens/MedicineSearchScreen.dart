6725de130c515ba92964aa1ce7328175685bf6d1
kieuvantuyen01 <tuyenkieuvan@gmail.com>
2022-04-22 17:25:00 +0700

    before merge

--- lib/screens/MedicineSearchScreen.dart
+++ lib/screens/MedicineSearchScreen.dart
@@ -12,34 +12,35 @@ class MedicineSearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: SmallHeaderWidget(
-          text: address,
-          icon: const Icon(
-            Icons.location_on,
-            color: Colors.black,
-          ),
+      appBar: SmallHeaderWidget(
+        text: address,
+        icon: const Icon(
+          Icons.location_on,
+          color: Colors.black,
         ),
+      ),
       body: Center(
         child: StreamBuilder<QuerySnapshot>(
-          stream: fetchProducts(), builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-          if(snapshot.hasData) {
-            print('check ok');
-            List<DocumentSnapshot>
-            documents = snapshot.data!.docs;
-            List<Pharmacy> items = <Pharmacy>[];
-            for(var i = 0; i < documents.length; i++) {
-              DocumentSnapshot document = documents[i];
-              items.add(Pharmacy.fromMap(document.data() as Map<dynamic, dynamic>));
-              print(items.first);
+          stream: fetchProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+            if (snapshot.hasData) {
+              print('check ok');
+              List<DocumentSnapshot> documents = snapshot.data!.docs;
+              List<Pharmacy> items = <Pharmacy>[];
+              for (var i = 0; i < documents.length; i++) {
+                //print(documents[i].data().toString());
+                DocumentSnapshot document = documents[i];
+                items.add(
+                    Pharmacy.fromMap(document.data() as Map<dynamic, dynamic>));
+                print(items.first);
+              }
+              return PharmacyBoxList(items: items);
+            } else {
+              print('no data');
+              return const Center(child: CircularProgressIndicator());
             }
-            return PharmacyBoxList(items: items);
-          } else {
-            print('no data');
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+          },
         ),
-      )
-      );
+      ));
 }
